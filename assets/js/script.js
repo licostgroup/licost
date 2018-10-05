f05354a238e37b0f4cb59aab25512c25f86cb5c4
Gleb Irovich <ch.cookiehunter@gmail.com>
2018-10-05 19:46:02 +0200

    template is ready

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+
+$('.slide').click(function (e) {
+    e.preventDefault();
+    console.log('Slide');
+    
+	var aid = $(this)
+		.attr('href')
+	$('html,body').animate({ scrollTop: $(aid).offset().top }, 'slow');
+});
+
